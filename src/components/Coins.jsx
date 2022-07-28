78e2d2f8207e52517b63fc1f765435814c81a300
Morteza <mtoshe87@gmail.com>
2022-07-28 22:18:29 +0430

    crypto api react app was created!

---
+++ src/components/Coins.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import CoinItem from './CoinItem';
+import './coins.css';
+
+const Coins = (props) => {
+  return (
+    <div className='cointainer'>
+        <div>
+            <div className="heading">
+                <p>#</p>
+                <p className='coin-name'>Coin</p>
+                <p>Price</p>
+                <p>24H</p>
+                <p className='hide-mobile'>Volume</p>
+                <p className='hide-mobile'>Market Cap</p>
+            </div>
+
+            {
+                props.coins.map(coins => {
+                    return(
+                        <CoinItem coins={coins} key={coins.id}/>
+                    )
+                })
+            }
+        </div>
+
+    </div>
+  )
+}
+
+export default Coins
